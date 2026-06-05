7d64bd9441b6db2148fd8b1fc7f5d4c78d301b8c
lilikong <lilikong@qti.qualcomm.com>
2026-06-04 17:00:11 -0700

    Add repolint.json to exclude 3rd party files from license check and fix OSR concerns
    Signed-off-by: lilikong <lilikong@qti.qualcomm.com>

--- src/qds/libusb/source/libusb-dynamic-loader.h
+++ src/qds/libusb/source/libusb-dynamic-loader.h
@@ -1,3 +1,5 @@
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD 3-Clause Clear License
 #ifndef LIBUSB_DYNAMIC_LOADER
 #define LIBUSB_DYNAMIC_LOADER
 
